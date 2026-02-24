7eb0a542c5b01f53f894e8d8848420e0f17a1525
chakriswamireddy <iamthechakri@gmail.com>
2026-02-24 17:34:37 +0530

    fix- new user stream issue

--- index.ts
+++ index.ts
@@ -1,32 +1,3 @@
-// import express from "express"
-// import mongoose from "mongoose"
-// import cors from "cors"
-// import dotenv from "dotenv"
-
-// import userRoutes from "./routes/users"
-// import roomRoutes from "./routes/rooms"
-// import interactionRoutes from "./routes/interactions"
-
-// dotenv.config()
-
-// const app = express()
-
-// app.use(cors())
-// app.use(express.json())
-
-// mongoose
-//   .connect(process.env.MONGO_URI as string)
-//   .then(() => console.log("MongoDB connected"))
-//   .catch(console.error)
-
-// app.use("/users", userRoutes)
-// app.use("/rooms", roomRoutes)
-// app.use("/interactions", interactionRoutes)
-
-// const PORT = process.env.PORT || 4000
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
 
 
 import express, { Request, Response } from "express"
@@ -54,7 +25,7 @@ initSocket(server)
 
 
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
   console.log("Server running on", PORT)
 })
--- socket.ts
+++ socket.ts
@@ -1,73 +1,48 @@
-import { Server, Socket } from "socket.io";
-import http from "http";
-
-type PlayerMap = {
-  [socketId: string]: {
-    peerId: string;
-  };
-};
-
-const rooms: Record<string, PlayerMap> = {};
+import { Server, Socket } from "socket.io"
+import http from "http"
 
 export function initSocket(server: http.Server) {
   const io = new Server(server, {
+    transports: ["websocket", "polling"],
     cors: {
       origin: "*",
       methods: ["GET", "POST"],
       credentials: true,
     },
-    transports: ["websocket", "polling"],
-  });
+  })
 
   io.on("connection", (socket: Socket) => {
-    console.log("Socket connected:", socket.id);
+    console.log("Server is Connected", socket.id)
 
-    socket.on("join-room", (roomId: string, peerId: string) => {
-      console.log(`${peerId} joined room ${roomId}`);
+    socket.on("join-room", (roomId: string, userId: string) => {
+      console.log(`${userId} a new user joined in room ${roomId}`)
 
-      socket.join(roomId);
+      socket.join(roomId)
 
-      if (!rooms[roomId]) {
-        rooms[roomId] = {};
-      }
 
-      rooms[roomId][socket.id] = { peerId };
+      socket.to(roomId).emit("user-connected", userId)
+    })
 
-      socket.to(roomId).emit("user-joined", {
-        socketId: socket.id,
-        peerId,
-      });
+    socket.on("user-toggle-audio", (userId: string, roomId: string) => {
+      console.log(userId, "want to toggle audio", roomId)
 
-      io.to(roomId).emit("players", rooms[roomId]);
-    });
+      socket.to(roomId).emit("user-toggle-audio", userId)
+    })
 
-    socket.on("user-toggle-audio", (peerId: string, roomId: string) => {
-      socket.to(roomId).emit("user-toggle-audio", peerId);
-    });
+    socket.on("user-toggle-video", (userId: string, roomId: string) => {
+      console.log(userId, "want to toggle video", roomId)
 
-    socket.on("user-toggle-video", (peerId: string, roomId: string) => {
-      socket.to(roomId).emit("user-toggle-video", peerId);
-    });
+      socket.to(roomId).emit("user-toggle-video", userId)
+    })
 
-    socket.on("user-leave", (peerId: string, roomId: string) => {
-      socket.to(roomId).emit("user-leave", peerId);
-      socket.leave(roomId);
-    });
+    socket.on("user-leave", (userId: string, roomId: string) => {
+      console.log(userId, "want to leave this room", roomId)
+
+      socket.to(roomId).emit("user-leave", userId)
+    })
 
     socket.on("disconnect", () => {
-      console.log("Socket disconnected:", socket.id);
-
-      for (const roomId in rooms) {
-        if (rooms[roomId][socket.id]) {
-          delete rooms[roomId][socket.id];
-
-          io.to(roomId).emit("players", rooms[roomId]);
-
-          if (Object.keys(rooms[roomId]).length === 0) {
-            delete rooms[roomId];
-          }
-        }
-      }
-    });
-  });
+      console.log("Client disconnected", socket.id)
+    })
+  })
 }
